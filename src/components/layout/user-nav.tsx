315e0bffa9783cff0902ce2d47ced750c7ae1bc9
ColeGN <gantogtokh0221@gmail.com>
2025-08-02 10:20:45 +0100

    hello wold

--- src/components/layout/user-nav.tsx
+++ src/components/layout/user-nav.tsx
@@ -12,9 +12,12 @@ import {
 import { UserAvatarProfile } from '@/components/user-avatar-profile';
 import { SignOutButton, useUser } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
+import { useT } from '@/hooks/use-t';
+import Profile from '../translation/profile';
 export function UserNav() {
   const { user } = useUser();
   const router = useRouter();
+  const t = useT();
   if (user) {
     return (
       <DropdownMenu>
@@ -42,7 +45,7 @@ export function UserNav() {
           <DropdownMenuSeparator />
           <DropdownMenuGroup>
             <DropdownMenuItem onClick={() => router.push('/dashboard/profile')}>
-              Profile
+              {t('Profile')}
             </DropdownMenuItem>
             <DropdownMenuItem>Billing</DropdownMenuItem>
             <DropdownMenuItem>Settings</DropdownMenuItem>
