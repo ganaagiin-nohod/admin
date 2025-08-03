44cec19c612463a3395a61c0841717862aacc668
ColeGN <gantogtokh0221@gmail.com>
2025-08-03 10:32:04 +0100

    little changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ const META_THEME_COLORS = {
 };
 
 export const metadata: Metadata = {
-  title: 'Next Shadcn',
-  description: 'Basic dashboard with Next.js and Shadcn'
+  title: 'Ganas Saas',
+  description: ''
 };
 
 export const viewport: Viewport = {
